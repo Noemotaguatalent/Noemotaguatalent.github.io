0304efc2698c6354d5935ebf32f4191fc7f314e8
Noemotaguatalent <noe.mercadomartinez@jpschools.org>
2025-09-18 17:00:48 +0000

    Platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,24 +27,40 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+    toggleGrid();
 
     // TODO 2 - Create Platforms
-
-
-
-
+    createPlatform(0, 0, 10, 0);
+    createPlatform(300, 650, 100, 50, "red");
+    createPlatform(600, 550, 115, 50, "Blue");
+    createPlatform(800, 500, 100, 50, "green");
+    createPlatform(600, 400, 100, 50, "red");
+    createPlatform(0, 500, 100, 50, "purple");
+    createPlatform(400, 300, 30, 50, "cyan");
+    createPlatform(600, 200, 30, 50, "dark green");
+    createPlatform(800, 300, 50, 10, "cyan");
+    createPlatform(900, 300, 30, 90, "cyan");
+    createPlatform(900, 390, 130, 40, "cyan");
+    createPlatform(1000, 300, 30, 90, "cyan");
+    createPlatform(1000, 300, 100, 10, "cyan");
+    createPlatform(1100, 300, 30, 200, "cyan");
+    createPlatform(1100, 500, 100, 20, "cyan");
+    createPlatform(1200, 390, 30, 190, "pink");
+    createPlatform(800, 250, 30, 100, "pink");
+    createPlatform(1200, 400, 100, 10, "yelow");
+    createPlatform(1300, 700, 100, 10, "blue");
     // TODO 3 - Create Collectables
-
-
-
-    
+    createCollectable("steve", 1140, 450);
+    createCollectable("diamond", 600, 500, 0.5, 0.7);
+    createCollectable("grace", 400, 299);
+    createCollectable("database", 1300, 600);
     // TODO 4 - Create Cannons
+    createCannon("top", 200, 990);
+    createCannon("right", 300, 2000);
+    createCannon("top", 799, 1400);
+    createCannon("top", 1220, 1990);
+    createCannon("right", 790, 100);
 
-
-    
-    
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
